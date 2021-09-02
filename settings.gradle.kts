4897fe9fd25bc7b8c59ef5608b8252f9557a766b
Muhammad Shiddiq Fathullah <msf31@student.gunadarma.ac.id>
2021-09-02 14:13:44 +0700

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,3 @@
+
+rootProject.name = "Gits-Task"
+
---
+++ src/main/kotlin/Tugas2.kt
@@ -0,0 +1,20 @@
+import java.util.*
+
+//simple sorting and classified to even or odd numbers
+
+fun main(args: Array<String>) {
+    val s = Scanner(System.`in`)
+    print("Input your numbers = ")
+    val n: String = s.nextLine()
+    val num = n.split(" ").toTypedArray()
+    num.sort()
+    println("Sorted = " + num.contentToString())
+    for (x in num.indices) {
+        if (num[x].toInt() % 2 == 0) {
+            println(num[x] + " is Even")
+        } else {
+            println(num[x] + " is Odd")
+        }
+    }
+    s.close()
+}
