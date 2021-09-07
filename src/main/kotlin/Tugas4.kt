60bf8cce9cf1e8087bcf73b7d45df3d50f39674f
Muhammad Shiddiq Fathullah <msf31@student.gunadarma.ac.id>
2021-09-07 14:07:39 +0700

    Add command in task 4

--- src/main/kotlin/Tugas4.kt
+++ src/main/kotlin/Tugas4.kt
@@ -1,14 +1,20 @@
 fun main(args: Array<String>) {
+    //kumpulan angka dalam array
     val arr = arrayListOf(
         12, 32, 23, 42, 34, 63, 39, 36, 85, 73, 90,
         23, 63, 34, 72, 89, 10, 93, 62, 82, 43, 62,
         48, 49, 30, 86, 63, 62, 39, 92, 37, 67, 98,
         20, 17, 97, 83, 73, 11, 23, 29, 53, 67, 41,
     )
+    /*
+    mencetak array yang sudah dijalankan function
+    sort dan calculate.
+     */
     println(sort(arr))
     print(calculate(arr))
 }
 
+//function sort untuk dapat mengurutkan array secara asc
 fun sort(x: ArrayList<Int>): ArrayList<Int> {
     var n = 0
     while (n < x.size) {
@@ -24,6 +30,9 @@ fun sort(x: ArrayList<Int>): ArrayList<Int> {
     return x
 }
 
+/*function calculate untuk menghitung jumlah angka
+genap dan ganjil, sekaligus menghitung total semua
+angka jika dijumlahkan */
 fun calculate(x: ArrayList<Int>): String {
     var numEven = 0
     var numOdd = 0
